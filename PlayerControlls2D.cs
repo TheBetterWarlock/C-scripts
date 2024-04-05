f542ff5e066231d4896264f16fe91961e9a6d5ce
TheBetterWarlock <145024640+TheBetterWarlock@users.noreply.github.com>
2024-04-05 12:06:41 +0200

    Add files via upload

---
+++ PlayerControlls2D.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlls2D : MonoBehaviour
+{
+    [SerializeField] private float playerMovementSpeed = 1f;
+    private int facingInt;
+
+    private void Update()
+    {
+        // Horizontal Movement
+
+        GetAxis() {
+            transform.position.x = transform.position.x * playerMovementSpeed * //axis
+            if (/*axis*/ < -0.1f)
+            {
+                facingInt = -1;
+            } else if (/*axis*/ > 0.1f)
+            {
+                facingInt = 1;
+            }
+        }
+
+        // Vertical Movement
+
+        GetKeyDown() {
+            if (OnGroundChecker())
+            {
+                // Add vertical force
+            }
+        }
+
+        // Attacking
+
+        GetMouseDown () {
+            // 
+        }
+    }
+
+    public bool OnGroundChecker()
+    {
+        // Checks for Contact with Ground
+
+
+    }
+}
---
+++ weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon
+{
+    string weaponName;
+    float weaponRange;
+    // weaponType (melee/ranged/...) (enum/string?)
+    float weaponDamage;
+    float weaponCoolDown;
+    // weaponDamageType
+}
